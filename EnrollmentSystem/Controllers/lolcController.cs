f6bb6ba0071685af30ddf9d208b0c8608384726b
Mark Josmil Degamo <MDegamo01@lear.com>
2022-08-24 14:08:44 +0800

    a

---
+++ EnrollmentSystem/Controllers/lolcController.cs
@@ -0,0 +1,50 @@
+﻿using EnrollmentSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EnrollmentSystem.Controllers
+{
+    public class lolcController : Controller
+    {
+        schoolEntities db = new schoolEntities();
+
+        // GET: lolc
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetCourses()
+        {
+            using (schoolEntities db = new schoolEntities())
+            {
+                var courses = db.records.ToList();
+                return Json(new { data = courses }, JsonRequestBehavior.AllowGet);
+            }
+
+        }
+
+
+        public ActionResult save(record rec)
+        {
+            
+            bool status = false;
+
+            if (ModelState.IsValid)
+            {
+                using (schoolEntities db = new schoolEntities())
+                {
+                    db.records.Add(rec);
+                    db.SaveChanges();
+                    status = true;
+                }
+
+               
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+    }
+}
